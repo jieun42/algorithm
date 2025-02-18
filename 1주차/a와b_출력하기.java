a903fcfa68bfbaccdb073b0eb8d31c50b06a4595
jieun42 <wise0402@naver.com>
2025-02-18 21:32:39 +0900

    1주차: a와b 출력하기

---
+++ 1주차/a와b_출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class a와b_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("a = %d\n", a);
+        System.out.printf("b = %d", b);
+
+        sc.close();
+    }
+}
